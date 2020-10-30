56ef8d39a4b7297ea2e589ff587f793101665ad5
Jesus Urbaneja <Urbah>
2020-10-30 17:52:20 -0300

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Counter!</h1>
+      <h3>Hour - Minutes - Seconds</h3>
+      <p className='time'>
+        <FontAwesomeIcon className='icon' icon={faClock} size="lg"/>   
+        {(props.hours>=10)?'':'0'}{props.hours} : 
+        {(props.mins>=10)?'':'0'}{props.mins} : 
+        {(props.segs>=10)?'':'0'}{props.segs}
+      </p>
+      <div className='btns'>
+        <button onClick={(e)=>{props.fnStop(e)}}>{props.runTime?'Stop':'Continue'}</button>
+        <button onClick={()=>{props.fnReset()}}>Reset</button>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let segs = 0;
+let mins = 0;
+let hours = 0;
+let runTime = true;
+function tick() {
+  segs++
+  function reset(){
+    segs = -1;
+    mins = 0;
+    hours = 0;
+    tick()
+ }
+    if(segs===60){
+      segs=0;
+      mins++
+      if(mins===60){
+        mins=0;
+        hours++;
+      }
+    }
+  function stop(e){
+    if(runTime){
+      runTime=false;
+      clearInterval(myTime)
+      segs-=1
+      tick()
+    }
+    else{
+      runTime=true;
+      myTime = setInterval(tick, 1000)
+      tick()
+    }
+  }
+  
+  const element = (
+    <div className='container'>
+      <App 
+      runTime={runTime}
+      segs={segs} mins={mins} hours={hours} 
+      fnReset={reset} fnStop={stop}
+      ></App>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let myTime = setInterval(tick, 1000);
